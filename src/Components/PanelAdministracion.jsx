1245c7803867aaf46686845a52547fee461c849a
IsmaelSailema20 <ismaelsailema110@gmail.com>
2025-04-17 03:11:50 -0500

    Creacion de la estructura del panel de administración

---
+++ src/Components/PanelAdministracion.jsx
@@ -0,0 +1,118 @@
+import { useState } from "react";
+import {
+  IconoCerrarSesion,
+  IconoEmpleados,
+  IconoHospital,
+  IconoPerfil,
+  IconoReportes,
+  IconoUsuarios,
+} from "../assets/IconosComponentes";
+
+const navItems = [
+  { key: "empleados", label: "Empleados", icon: <IconoEmpleados /> },
+  { key: "centroMedico", label: "Centros Médicos", icon: <IconoHospital /> },
+  { key: "reportes", label: "Reportes", icon: <IconoReportes /> },
+  {
+    key: "gestionUsuarios",
+    label: "Gestión de usuarios",
+    icon: <IconoUsuarios />,
+  },
+];
+
+const PanelAdministracion = () => {
+  const [activeSection, setActiveSection] = useState("principal");
+
+  const baseLinkClasses =
+    "flex items-center px-6 py-3 mb-4 text-text-primary transition-colors duration-300 transform rounded-md cursor-pointer hover:bg-hover-gray";
+
+  const renderSection = () => {
+    //AQUI VA CADA UNO DE LOS COMPONENTES QUE SE VAN A RENDERIZAR EN CADA UNA DE LAS SECCIONES
+    switch (activeSection) {
+      case "empleados":
+        return <h1>Componente Empleados</h1>;
+      case "centroMedico":
+        return <h1>Componente centro médico</h1>;
+      case "reportes":
+        return <h1>Componente Reportes</h1>;
+      case "gestionUsuarios":
+        return <h1>Gestión de Usuarios</h1>;
+      default:
+        return <h1>Sección: {activeSection}</h1>;
+    }
+  };
+
+  return (
+    <div className="flex">
+      {/* Sidebar */}
+      <aside className="flex flex-col w-80 h-screen px-4 py-8 overflow-y-auto bg-bg-primary shadow-lg">
+        <a href="#" className="flex items-center">
+          <img
+            className="w-auto h-6 sm:h-7"
+            src="Icons/LogoHospital.svg"
+            alt="Logo"
+          />
+          <span
+            className="mx-3 font-extrabold text-xl text-transparent bg-clip-text "
+            style={{
+              backgroundImage:
+                "linear-gradient(to left, rgba(49, 69, 185, 0.60), #0067D2)",
+            }}
+          >
+            Centro Hospitalario
+          </span>
+        </a>
+
+        <div className="flex flex-col justify-between gap-12 mt-6">
+          <nav>
+            {navItems.map(({ key, label, icon }) => (
+              <div
+                key={key}
+                onClick={() => setActiveSection(key)}
+                className={`${baseLinkClasses} ${
+                  activeSection === key ? "bg-hover-gray text-gray-700" : ""
+                }`}
+              >
+                <span className="mx-2">{icon}</span>
+                <span className="mx-4 font-semibold ">{label}</span>
+              </div>
+            ))}
+            <div className="border-t border-gray-300 mt-16 " />
+          </nav>
+
+          <div>
+            <h4 className="text-text-primary font-semibold text-base mb-2">
+              Perfil
+            </h4>
+
+            <div className="flex items-center gap-3 mb-4 mx-6 hover:">
+              <IconoPerfil />
+              <div>
+                <p className="text-text-primary font-semibold text-sm">
+                  Nombre usuario
+                </p>
+                <span className="text-text-secondary font-light text-sm">
+                  example@gmail.com
+                </span>
+              </div>
+            </div>
+          </div>
+          <div
+            onClick={() => console.log("Cerrar sesión")}
+            className={`${baseLinkClasses}  items-center justify-center`}
+          >
+            <IconoCerrarSesion />
+
+            <span className="mx-4 font-medium">Cerrar Sesión</span>
+          </div>
+        </div>
+      </aside>
+
+      {/* Contenido */}
+      <main className="flex-1 max-h-screen overflow-y-auto p-6">
+        {renderSection()}
+      </main>
+    </div>
+  );
+};
+
+export default PanelAdministracion;
