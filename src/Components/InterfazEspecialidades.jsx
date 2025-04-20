5d50be9e9fd483e2cc8838bb91b367b0875f7974
W1llAn <wchimborazo2840@uta.edu.ec>
2025-04-20 13:02:11 -0500

    Creacion del componente para la interfaz de especialidades

--- src/Components/InterfazEspecialidades.jsx
+++ src/Components/InterfazEspecialidades.jsx
@@ -3,19 +3,16 @@ import TablaDatos from "./TablaDatos";
 import api from "../api/config";
 
 const InterfazEmpleados = () => {
-  const [especialidades, setEspecialidades] = useState();
+  //
+  const [especialidades, setEspecialidades] = useState([]);
+
   const columnas = [
     {
-      title: "Especialidad",
-      data: "",
+      title: "especialidad",
+      data: "especialidad",
       className: "font-medium text-gray-900",
     },
-    {
-      title: "Acciones",
-      className: "text-gray-600",
-    },
   ];
-
   const fetchEspecialidades = async () => {
     try {
       const response = await api.get("/Especialidades");
@@ -27,7 +24,7 @@ const InterfazEmpleados = () => {
 
   useEffect(() => {
     fetchEspecialidades();
-  }, []);
+  });
 
   const opcionesTabla = {
     paging: true,
@@ -40,15 +37,15 @@ const InterfazEmpleados = () => {
   return (
     <>
       <div className="flex justify-between items-center mb-6 px-4 py-3 bg-gray-50 rounded-lg border border-gray-200">
-        <h1 className="text-2xl font-bold text-gray-800">Empleados</h1>
+        <h1 className="text-2xl font-bold text-gray-800">Especialidades</h1>
         <button className="px-4 py-2 bg-blue-600 text-white rounded-md border border-blue-700 hover:bg-blue-700 transition-colors shadow-sm text-sm font-medium">
-          Agregar Empleado
+          Agregar Especialidad
         </button>
       </div>
       <div>
         <TablaDatos
           columnas={columnas}
-          data={especialidades}
+          datos={especialidades}
           opciones={opcionesTabla}
         />
       </div>
