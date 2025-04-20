425e7c42fe1aa6407c272b1fa8d9e68ab729a147
IsmaelSailema20 <ismaelsailema110@gmail.com>
2025-04-20 16:34:10 -0500

    Implementacion de rutas en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import PanelAdministracion from "./Components/PanelAdministracion";
-
+import Login from "./Components/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PrimeReactProvider } from "primereact/api";
+import PanelAdministracion from "./Components/PanelAdministracion";
+import ProtectedRoute from "./Components/ProtectedRoute";
 function App() {
   return (
     <>
       <PrimeReactProvider>
-        <PanelAdministracion />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />}></Route>
+            <Route element={<ProtectedRoute />}>
+              <Route
+                path="/administracion"
+                element={<PanelAdministracion />}
+              ></Route>
+            </Route>
+
+            <Route path="/" element={<Login />}></Route>
+          </Routes>
+        </BrowserRouter>
       </PrimeReactProvider>
     </>
   );
